ec312c5d81f23c4e1899f5ec171cffd7d6d02162
sujanchhengutala9@gmail.com <sujanchhengutala9@gmail.com>
2021-06-27 13:59:17 +0545

    challenge2

---
+++ code challenge2/script.js
@@ -0,0 +1,13 @@
+/*1. Print a nice output to the console, saying who has the higher BMI. The message is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's BMI (28.3) is higher than John's (23.9)!"*/
+
+
+let markBMI = 27.309968138370508;
+let johnBMI = 24.194608809993426;
+if(markBMI > johnBMI){
+  console.log(`mark has higher BMI(27.309968138370508)  than jhon (24.194608809993426)`);
+}
+else{
+  console.log(`john has higher BMI (24.194608809993426) than mark(27.309968138370508)`);
+}
